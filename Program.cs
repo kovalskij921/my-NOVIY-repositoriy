5e72b940de0bebb9d352f1f39dfe54e37fca8f72
Kovalskij921 <kovalskij97@mail.ru>
2022-01-27 23:09:09 +0300

    commit 1

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str;
+            int a, b, c;
+            Console.WriteLine("Стоимость плитки шоколада");
+            str = Console.ReadLine();
+            a = Convert.ToInt32(str);
+            Console.WriteLine("Стоимость кофе");
+            str = Console.ReadLine();
+            b = Convert.ToInt32(str);
+            Console.WriteLine("Стоимость пакета молока");
+            str = Console.ReadLine();
+            c = Convert.ToInt32(str);
+            int result = a + b + c;
+            Console.WriteLine("Общая стоимость продуктов = " + result);
+            Console.Read();
+        }
+    }
+}
