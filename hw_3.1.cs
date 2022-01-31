79f586c509fe221d92c0ce1c5072b3183e45779b
Kovalskij921 <kovalskij97@mail.ru>
2022-01-31 18:02:07 +0300

    2Commit

---
+++ hw_3.1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hw_3._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число: ");
+            int number;
+            number = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Result: {number}");
+            Console.Read();
+        }
+    }
+}
