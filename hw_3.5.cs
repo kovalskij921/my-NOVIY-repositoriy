e89dd56c7cd278332f4b74fc69bad71678cd7527
Kovalskij921 <kovalskij97@mail.ru>
2022-01-31 18:38:35 +0300

    4commit

---
+++ hw_3.5.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hw_3._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hi, I'm Mr. Program, enter any number and it will be output by the console");
+            string anyword;
+            anyword = (Console.ReadLine());
+            Console.WriteLine($"Result is: {anyword}");
+            Console.Read();
+        }
+    }
+}
