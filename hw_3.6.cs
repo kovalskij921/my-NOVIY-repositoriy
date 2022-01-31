2abb77a36174cab2ed961faca92fc056b6bc9fa6
Kovalskij921 <kovalskij97@mail.ru>
2022-01-31 18:44:58 +0300

    4commit

---
+++ hw_3.6.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hw_3._6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number: ");
+            int numberOne;
+            numberOne = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter a number: ");
+            int numberTwo;
+            numberTwo = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter a number: ");
+            int numberThree;
+            numberThree = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter a number: ");
+            int numberFour;
+            numberFour = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"{numberOne} {numberTwo} {numberThree} {numberFour}");
+            Console.Read();
+
+        }
+    }
+}
