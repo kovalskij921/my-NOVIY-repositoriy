e276f7621438112663b8f75d55122fb427bc6c56
Kovalskij921 <kovalskij97@mail.ru>
2022-02-01 19:26:00 +0300

    commit5

---
+++ hw_3.4.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hw_3._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double result = Math.Round(Math.E, 1);
+            Console.WriteLine($"This is a natural logariphm: {Math.Round(Math.E, 1)}");
+            Console.Read();
+        }
+    }
+}
