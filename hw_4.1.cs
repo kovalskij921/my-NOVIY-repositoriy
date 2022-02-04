7b4d2c8d4cb1d0f38bd2ab8027b97f2c9f6937eb
Kovalskij921 <kovalskij97@mail.ru>
2022-02-04 18:21:08 +0300

    5commit

---
+++ hw_4.1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hw_4._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, c, Result, square;  // I used "bukavi" because it's more convenient that way
+            Console.WriteLine("Enter a number 1");
+            a = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter a number 2");
+            b = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter a number 3");
+            c = double.Parse(Console.ReadLine());
+            square = (Math.Pow((c + (a * b) / c), 2));
+            Result = a + b - c * ((3 * a * b) + (Math.Pow(a, 2)) / (b * c)) - (c + (a * b) / c) * square;
+            Math.Round(Result, 2);
+            Console.WriteLine($"Answer {Math.Round(Result, 2)}");
+            Console.Read();
+        }
+    }
+}
