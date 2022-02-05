521746c6908232a225637ac602c6b83527b220c1
Kovalskij921 <kovalskij97@mail.ru>
2022-02-05 15:29:27 +0300

    commit

---
+++ hw_4.2.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hw_4._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ab;
+            Console.WriteLine("Введите число");
+
+
+            ab = Console.ReadLine();
+
+            if (ab == "1")
+            {
+                Console.WriteLine("А");
+            }
+            else if (ab == "2")
+            {
+                Console.WriteLine("Б");
+            }
+            else if (ab == "3")
+            {
+                Console.WriteLine("В");
+            }
+            else if (ab == "4")
+            {
+                Console.WriteLine("Г");
+            }
+            else if (ab == "5")
+            {
+                Console.WriteLine("Д");
+            }
+            else if (ab == "6")
+            {
+                Console.WriteLine("Е");
+            }
+            else if (ab == "7")
+            {
+                Console.WriteLine("Ё");
+            }
+            else if (ab == "8")
+            {
+                Console.WriteLine("Ж");
+            }
+            else if (ab == "9")
+            {
+                Console.WriteLine("З");
+            }
+            else if (ab == "10")
+            {
+                Console.WriteLine("И");
+            }
+            else if (ab == "11")
+            {
+                Console.WriteLine("Й");
+            }
+            else if (ab == "12")
+            {
+                Console.WriteLine("К");
+            }
+            else if (ab == "13")
+            {
+                Console.WriteLine("Л");
+            }
+            else if (ab == "14")
+            {
+                Console.WriteLine("М");
+            }
+            else if (ab == "15")
+            {
+                Console.WriteLine("Н");
+            }
+            else if (ab == "16")
+            {
+                Console.WriteLine("О");
+            }
+            else if (ab == "17")
+            {
+                Console.WriteLine("П");
+            }
+            else if (ab == "18")
+            {
+                Console.WriteLine("Р");
+            }
+            else if (ab == "19")
+            {
+                Console.WriteLine("С");
+            }
+            else if (ab == "20")
+            {
+                Console.WriteLine("Т");
+            }
+            else if (ab == "21")
+            {
+                Console.WriteLine("У");
+            }
+            else if (ab == "22")
+            {
+                Console.WriteLine("Ф");
+            }
+            else if (ab == "23")
+            {
+                Console.WriteLine("Х");
+            }
+            else if (ab == "24")
+            {
+                Console.WriteLine("Ц");
+            }
+            else if (ab == "25")
+            {
+                Console.WriteLine("Ч");
+            }
+            else if (ab == "26")
+            {
+                Console.WriteLine("Ш");
+            }
+            else if (ab == "27")
+            {
+                Console.WriteLine("Щ");
+            }
+            else if (ab == "28")
+            {
+                Console.WriteLine("Ъ");
+            }
+            else if (ab == "29")
+            {
+                Console.WriteLine("Ы");
+            }
+            else if (ab == "30")
+            {
+                Console.WriteLine("Ь");
+            }
+            else if (ab == "31")
+            {
+                Console.WriteLine("Э");
+            }
+            else if (ab == "32")
+            {
+                Console.WriteLine("Ю");
+            }
+            else if (ab == "33")
+            {
+                Console.WriteLine("Я");
+            }
+            else
+            {
+                Console.WriteLine($" {ab}");
+            }
+
+
+
+            Console.Read();
+        }
+    }
+}
