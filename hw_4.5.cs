ac81e80d6f49e338d54ad630dd4e24c1aa16a952
Kovalskij921 <kovalskij97@mail.ru>
2022-02-07 16:38:07 +0300

    2commit

---
+++ hw_4.5.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hw_4._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double Celsius, Kelvin, Fahrenheit;
+
+            Console.WriteLine("Enter the temperature value on the Celsius scale");
+            Celsius = double.Parse(Console.ReadLine());
+
+            Fahrenheit = Celsius * 1.8;
+            Kelvin = Celsius + 273.15;
+
+            Console.WriteLine($"Result: {Celsius} degrees Celsius, {Kelvin} degrees Kelvin, { Fahrenheit} degrees Fahrenheit");
+            Console.Read();
+        }
+    }
+}
