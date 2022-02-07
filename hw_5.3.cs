cab7ef11e1a60acdaf56f2a796c1b741a50163d9
Kovalskij921 <kovalskij97@mail.ru>
2022-02-07 18:28:31 +0300

    commit

---
+++ hw_5.3.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hw_5._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("enter a number 1");
+                int x = int.Parse(Console.ReadLine());
+                Console.WriteLine("enter a number 2");
+                int y = int.Parse(Console.ReadLine());
+
+                if (x + y == 98)
+                {
+                    Console.WriteLine("True");
+                }
+                else if (x + y != 98)
+                {
+                    Console.WriteLine("False");
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("error, enter a number");
+            }
+            Console.Read();
+        }
+    }
+    }
+
