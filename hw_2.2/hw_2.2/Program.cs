3a5c8e387dee92001a0fadb244f9b1cdac405a5b
Kovalskij921 <kovalskij97@mail.ru>
2022-02-07 17:52:01 +0300

    c

---
+++ hw_2.2/hw_2.2/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hw_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите свое имя: ");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Введите свою фамилию: ");
+            string surname = Console.ReadLine();
+
+            Console.WriteLine("Введите свой возрас: ");
+            int age, b;
+            age = int.Parse(Console.ReadLine());
+            age2 = 2022 - age;
+            Console.WriteLine(age2);
+
+            Console.WriteLine("Введите свой род деятельности: ");
+            string activity = Console.ReadLine();
+
+            Console.WriteLine("Вы женаты? (введите <да> или <нет>: ");
+            string name = "да";
+            if (name == Console.ReadLine())
+            {
+                Console.WriteLine($" true");
+            }
+            else if (name != Console.ReadLine())
+            {
+                Console.WriteLine($"false");
+            }
+
+
+
+
+
+
+            Console.WriteLine($"женат: {name}");
+            Console.WriteLine($"Имя: {surname}");
+            Console.WriteLine($"Возраст: {age2}");
+            Console.WriteLine($"Вес: {activity}");
+            Console.WriteLine($"Работает: {name}");
+            Console.Read();
+
+
+
+
+        }
+    }
+}
