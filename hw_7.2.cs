02e9e56802452173700fb54f171ebd2f9f8735c1
Kovalskij921 <kovalskij97@mail.ru>
2022-07-09 15:58:48 +0300

    remote

---
+++ hw_7.2.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal x, y;
+            Console.Write("enter x \t");
+            x = int.Parse(Console.ReadLine());
+            Console.Write("enter y \t");
+            y = int.Parse(Console.ReadLine());
+            if (x > y)
+            {
+                Console.WriteLine("x - a larger number, y - smaller number");
+            }
+            else if (x < y)
+            {
+                Console.WriteLine("x - smaller number, y - a larger number");
+            }
+            else if (x == y)
+            {
+                Console.WriteLine("x is equal to y");
+            }
+            Console.Read();
+        }
+    }
+}
