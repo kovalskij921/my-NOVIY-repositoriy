ce84b44ea71fcd49d1be1a36ea5d6f06b4e14703
Kovalskij921 <kovalskij97@mail.ru>
2022-07-09 15:25:24 +0300

    remote

---
+++ hw_7.1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double y;
+            int x;
+            Console.Write("enter x");
+            x = int.Parse(Console.ReadLine());
+            if (x > 0)
+            {
+                y = Math.Sin(x);
+            }
+            else
+            {
+                y = Math.Cos(x);
+            }
+            Console.WriteLine(y);
+            Console.Read();
+        }
+    }
+}
